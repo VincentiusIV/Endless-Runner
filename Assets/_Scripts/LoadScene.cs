f6f255808d5c2497ef19bea4e3a77cc17eb6bf62
Vincent Versnel <vincent@versnelhifi.nl>
2016-09-26 16:20:35 +0200

    Initial Commit

---
+++ Assets/_Scripts/LoadScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadScene : MonoBehaviour {
+
+    public string sceneName;
+
+    void OnMouseDown()
+    {
+        Application.LoadLevel(sceneName);
+    }
+
+    void OnMouseOver()
+    {
+
+    }
+}
